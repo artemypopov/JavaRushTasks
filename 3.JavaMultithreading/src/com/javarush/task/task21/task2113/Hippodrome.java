96fc3436b2c130b7e60ee293c822f8d032f2fc69
artemypopov <artemy.popov.dev@gmail.com>
2023-06-05 18:02:10 +0500

    Hippodrome.java: cоздан класс

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,2 +1,65 @@
-package com.javarush.task.task21.task2113;public class Hippodrome {
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+
+    private List<Horse> horses;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    static Hippodrome game;
+
+    static void main(String[] args) {
+
+        Horse horse1 = new Horse("Лошадь1", 3, 0);
+        Horse horse2 = new Horse("Лошадь2", 3, 0);
+        Horse horse3 = new Horse("Лошадь3", 3, 0);
+
+        List<Horse> horses = new ArrayList<>();
+        horses.add(horse1);
+        horses.add(horse2);
+        horses.add(horse3);
+
+        game = new Hippodrome(horses);
+
+        game.run();
+
+    }
+
+    void run() {
+        for (int i = 0; i < 100 ; i++) {
+            move();
+            print();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public void move() {
+        for (int i = 0; i < horses.size(); i++) {
+            getHorses().get(i).move();
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < horses.size(); i++) {
+            getHorses().get(i).print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+    
 }
