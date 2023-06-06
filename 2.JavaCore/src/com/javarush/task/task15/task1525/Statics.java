78d3b17933407186e649cfcf270ade4d319f098e
artemypopov <artemy.popov.dev@gmail.com>
2023-06-06 22:25:11 +0500

    06.06.2023

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    public static String FILE_NAME = "C:\\Users\\Artem\\Downloads\\Пароль.txt"; /* add the path to your source file here */
+
+
+}
