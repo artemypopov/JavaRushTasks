668e7e17daab3e9230bbdf99d5d4fab2fd355d32
artemypopov <artemy.popov.dev@gmail.com>
2023-06-06 19:12:57 +0500

    06.06.2023

--- 1.JavaSyntax/src/com/javarush/task/jdk13/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/jdk13/task08/task0812/Solution.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /* 
 Cамая длинная последовательность
@@ -13,6 +14,45 @@ Cамая длинная последовательность
 public class Solution {
     public static void main(String[] args) throws IOException {
         //напишите тут ваш код
+        List<Integer> list = new ArrayList<>();
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 10; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        int count = 1;
+        int maxCount = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (!Objects.equals(list.get(i), list.get(j))) {
+                    break;
+                }
+                count++;
+//                if (Objects.equals(list.get(i), list.get(j))) {
+//                    count++;
+//                }
+            }
+            if (maxCount < count) {
+                maxCount = count;
+//                System.out.println("В цикле :" + maxCount);
+            }
+            count = 1;
+        }
+        System.out.println(maxCount);
     }
 }
+/*
+89
+12
+12
+98
+128
+42
+98
+98
+98
+71
+*/
