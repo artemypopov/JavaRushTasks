5e67631d19e771ca6cff85157a46eaa50ad2fdff
artemypopov <artemy.popov.dev@gmail.com>
2023-06-25 19:55:00 +0500

    25.06.2023

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task19.task1904;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/* 
+И еще один адаптер
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("тут путь к файлу который надо прочитать");
+        Scanner scanner = new Scanner(file);
+
+    }
+
+    public static class PersonScannerAdapter implements PersonScanner {
+
+        private Scanner fileScanner;
+
+        public PersonScannerAdapter(Scanner fileScanner) {
+            this.fileScanner = fileScanner;
+        }
+
+        @Override
+        public Person read() throws IOException {
+
+            return null;
+        }
+
+        @Override
+        public void close() throws IOException {
+            fileScanner.close();
+        }
+    }
+}
